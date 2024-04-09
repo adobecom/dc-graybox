a2fa8ad4d3593c43202b43320928be84f891d140
Arshad Mohammed <87503056+arshadparwaiz@users.noreply.github.com>
2024-04-09 16:04:55 -0700

    Repo Sync: homepage to dc-graybox

---
+++ test/scripts/scripts.test.js
@@ -0,0 +1,41 @@
+import { expect } from '@esm-bundle/chai';
+import * as sinon from 'sinon';
+import { waitForElement, delay } from '../helpers/waitfor.js';
+
+document.head.innerHTML = '<meta name="promotion" content="abc"/><meta name="dc-widget-version" content="123"/>';
+document.body.innerHTML =
+  '<header><main><div class="promotion"/><div class="dc-converter-widget"/></main></header>';
+
+const { scripts } = await import('../../acrobat/scripts/scripts');
+
+describe('Test scripts.js', () => {
+  let clock;
+
+  before(() => {
+    clock = sinon.useFakeTimers();
+  });
+
+  afterEach(() => {
+    sinon.resetHistory();
+  });
+
+  after(() => {
+    clock.restore();
+    sinon.restore();
+  }); 
+
+  it('triggers events', async function () {
+    window.adobeIMS = {
+      initialized: true,
+    };
+    window.dc_hosted = true;
+    window.browser = true;
+    window._satellite = {
+      track: sinon.spy(),
+    };
+
+    clock.tick(1100);
+  
+    expect(window.browser).to.be.true;
+  });
+});
